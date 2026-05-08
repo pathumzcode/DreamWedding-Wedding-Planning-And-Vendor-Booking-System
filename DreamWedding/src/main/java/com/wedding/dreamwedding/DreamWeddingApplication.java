32b7ddb10dd01e66df08dcaffc1cee04d10f52b2
Pathum Lakshan Bandara <198687174+pathumzcode@users.noreply.github.com>
2026-05-08 23:24:46 +0530

    File structure test

---
+++ DreamWedding/src/main/java/com/wedding/dreamwedding/DreamWeddingApplication.java
@@ -0,0 +1,13 @@
+package com.wedding.dreamwedding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamWeddingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamWeddingApplication.class, args);
+    }
+
+}
